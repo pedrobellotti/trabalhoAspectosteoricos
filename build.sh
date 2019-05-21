c3e7b83ac0598ba79ecabfa145a65eabd5d97923
Pedro <bellottipcpedro@gmail.com>
2019-05-21 18:34:53 -0300

    Alterando arquivo build

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 if command -v python3 &>/dev/null; then
     if [ -f main.py -a -f gerenciaArquivo.py -a -f validacaoTags.py ]; then
         python3 main.py
-        py3clean .
+	find . -type f -name "*.py[co]" -delete
+        find . -type d -name "__pycache__" -delete
     else
         echo [ERROR] Algum arquivo esta faltando! Arquivos necessarios: main.py gerenciaArquivo.py validacaoTags.py
     fi
