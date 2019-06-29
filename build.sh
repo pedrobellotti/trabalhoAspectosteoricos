326b039da3bde75725d8e319aec0ac1bc4840165
Pedro <bellottipcpedro@gmail.com>
2019-06-29 11:46:29 -0300

    Atualizando arquivo de build

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if command -v python3 &>/dev/null; then
-    if [ -f main.py -a -f gerenciaArquivo.py -a -f validacaoTags.py ]; then
+    if [ -f main.py -a -f gerenciaArquivo.py -a -f validacaoTags.py -a -f automato.py -a -f estado.py -a -f fabricaAutomato.py -a -f transicao.py ]; then
         python3 main.py
 	find . -type f -name "*.py[co]" -delete
         find . -type d -name "__pycache__" -delete
     else
-        echo [ERROR] Algum arquivo esta faltando! Arquivos necessarios: main.py gerenciaArquivo.py validacaoTags.py
+        echo [ERROR] Algum arquivo esta faltando! Arquivos necessarios: main.py gerenciaArquivo.py validacaoTags.py automato.py estado.py fabricaAutomato.py transicao.py
     fi
 else
     echo [ERROR] Python 3 nao instalado no computador!
