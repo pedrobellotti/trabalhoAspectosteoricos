cbddbc9414cd2700136a69d73d7e07fb13c73ea1
Pedro <bellottipcpedro@gmail.com>
2019-06-30 15:10:32 -0300

    Adicionando warn na sobreposicao de tags

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if command -v python3 &>/dev/null; then
-    if [ -f main.py -a -f gerenciaArquivo.py -a -f validacaoTags.py -a -f automato.py -a -f estado.py -a -f fabricaAutomato.py -a -f transicao.py -a -f divisaoTags.py]; then
+    if [ -f main.py -a -f gerenciaArquivo.py -a -f validacaoTags.py -a -f automato.py -a -f estado.py -a -f fabricaAutomato.py -a -f transicao.py -a -f divisaoTags.py ]; then
         python3 main.py
 	find . -type f -name "*.py[co]" -delete
         find . -type d -name "__pycache__" -delete
--- divisaoTags.py
+++ divisaoTags.py
@@ -9,10 +9,18 @@
 #Processa todas as palavras em todos os automatos e salva quais aceitaram
 def divideTag (lista_palavra, lista_automatos):
     aceito = []
+    quantidade_aceito = 0
+    sobreposicao = []
     #Percorre todas as palavras da lista
     for palavra in lista_palavra:
         for automato in lista_automatos:
             if (automato.aceita(palavra)):
+                quantidade_aceito += 1
                 aceito.append(automato.getTag()[:-1])
-    print (aceito)
+                sobreposicao.append(automato.getTag()[:-1])
+        if (quantidade_aceito > 1):
+            print ('[WARN] Sobreposicao na definicao das tags', *sobreposicao)
+        quantidade_aceito = 0
+        sobreposicao = []
+    print (*aceito)
     
